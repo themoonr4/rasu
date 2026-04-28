5d420c14f301d228d1f8ebfe3c80b8b09071684d
themoonr4 <themoonr4@gmail.com>
2026-04-28 17:08:07 +0530

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'the-moon-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/admin.html',
+  '/dashboard.html',
+  '/categories.html',
+  '/article.html',
+  '/settings.html',
+  '/bookmarks.html',
+  '/search.html',
+  '/profile.html',
+  '/privacy.html',
+  '/terms.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
